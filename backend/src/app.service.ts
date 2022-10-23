8f352ffd8a451e75fcd914f1afd77504a09ad4b8
GustavoDeMarinis <g.demarinis@opticpower.com>
2022-10-23 17:09:33 -0300

    Added Axios to package.json Added get method to retrieve commits made in this repo.

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { map, catchError } from 'rxjs';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private http: HttpService) {}
+  async getCommits() {
+    return this.http
+      .get(
+        'https://api.github.com/repos/GustavoDeMarinis/github-api-nestjs-reactJs/commits',
+      )
+      .pipe(
+        map((res) => {
+          return res?.data;
+        }),
+      )
+      .pipe(
+        catchError(() => {
+          throw new ForbiddenException('API not available');
+        }),
+      );
   }
 }
